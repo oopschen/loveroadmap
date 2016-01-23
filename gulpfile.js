6983d614806070c27e07c9a87ee96870c6d467b1
Ray <linxray@gmail.com>
2016-01-23 10:20:14 +0800

    remove gulp webpack use webpack-dev-server instead

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,6 @@ var WebpackDevServer = require("webpack-dev-server");
 // get webpack config
 var host='127.0.0.1', port= 8080;
 
-gulp.task('default', function() {
-  var webpackCfg = require('./webpack.config.js');
-  // Start a webpack-dev-server
-  new WebpackDevServer(webpack(webpackCfg), {
-      // server and middleware options
-  }).listen(port, host, function(err) {
-      if(err) throw new gutil.PluginError("webpack-dev-server", err);
-      // Server listening
-      gutil.log('[webpack-dev-server]', `http://${host}:${port}/webpack-dev-server/index.html`);
-  });
-});
-
 gulp.task('prod', function() {
   // set env
   process.env.NODE_ENV = 'production';
