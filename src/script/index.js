0d8063c4d54d7fce80f6758d4cd804311ccf18d8
Ray <linxray@gmail.com>
2016-02-19 16:23:35 +0800

    modify main page

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,1 @@
 require('css/bootstrap.css');
-require(['script/pathplayer'], function (PathPlayer) {
-  'use strict';
-  let pp = new PathPlayer({
-    target: '#container'
-  }, require('../data.json'));
-  pp.play();
-});
