9f71fa1195b7af2af9f5796f6c301aa9b05dcee0
Ray <linxray@gmail.com>
2016-03-06 11:00:16 +0800

    add letter && calculation

--- src/script/index.js
+++ src/script/index.js
@@ -1,12 +1,21 @@
 require('css/bootstrap.css');
 var mapHelper = require('script/maphelper');
-require(['jquery', 'data.json'], function($, pathData) {
-  var locData = [];
+var $ = require('jquery');
+require(['data.json'], function(pathData) {
+  var locData = [], locMap = {}, letterNum = 0;
   for (var i = 0; i < pathData.length; i++) {
     var data = pathData[i];
     if (!data.loc) {
       continue;
     }
+
+    // count letter num 
+    letterNum ++;
+
+    // count loc num
+    locMap[data.loc] = true;
+
+    // format location
     var locArr = data.loc.split(',');
     if (2 > locArr.length) {
       continue;
@@ -18,6 +27,20 @@ require(['jquery', 'data.json'], function($, pathData) {
     }, data));
 
   }
+
+  // count loc num
+  var locCnt = 0;
+  /* jshint ignore:start */
+  for (var j in locMap) {
+    locCnt ++;
+  }
+  /* jshint ignore:end */
+
+  // calculate time range
+  var startDateArr = pathData[0].date.split('.'),
+      endDateArr = pathData[pathData.length-1].date.split('.');
+  var timeRangeMS = new Date(startDateArr[0], startDateArr[1], startDateArr[2]).getTime() - 
+                      new Date(endDateArr[0], endDateArr[1], endDateArr[2]).getTime();
 
   var aniDrawPoint = function(mapIns, locData, i) {
       if (i >= locData.length) {
@@ -36,6 +59,11 @@ require(['jquery', 'data.json'], function($, pathData) {
   };
 
   $(function() {
+    // render num
+    $('#letterNum').html(letterNum);
+    $('#locNum').html(locCnt);
+    $('#timeRange').html(timeRangeMS/1000/60/60 + '小时');
+
     $('#nextBtn').click(function() {
       $('#preface').css('display', 'none');
       $('#map').css('display', 'block');
