9234fa12b3c5739f94b686bf78e4106be38f7da3
Ray <linxray@gmail.com>
2016-03-06 12:58:50 +0800

    add clean task and deploy

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gutil = require("gulp-util");
 var webpack = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
 var path = require('path');
+var ghPages = require('gulp-gh-pages');
+var del = require('del');
 
 // get webpack config
 var host='0.0.0.0', port= 8080;
@@ -12,16 +14,27 @@ gulp.task('default', function() {
    gutil.log("run command for development: webpack-dev-server --watch --hot --content-base src/ ");
 });
 
-gulp.task('prod', function() {
+gulp.task('prod', ['clean'], function() {
   // set env
   process.env.NODE_ENV = 'production';
   var webpackCfg = require('./webpack.config.js');
 
   // run webpack
   webpack(webpackCfg, function(err, stats) {
-    if(err) {
-      throw new gutil.PluginError("webpack", err);
+    if(err) { throw new gutil.PluginError("webpack", err);
     }
     gutil.log("[webpack]", stats.toString({}));
   });
+});
+
+
+gulp.task('deploy', function() {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages());
+});
+
+gulp.task('clean', function() {
+  return del([
+    './build/'
+  ]);
 });
